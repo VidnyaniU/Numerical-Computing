5a5d9fb2a9ff47cb4b1870b4ff1b40c83475da0e
VidnyaniU <vidnyani.umathe@gmail.com>
2024-01-24 13:18:03 +0530

    Updated Complex Numbers

--- Complex Numbers/complexNumber.cpp
+++ Complex Numbers/complexNumber.cpp
@@ -4,17 +4,20 @@
 
 using namespace std;
 
+// default constructor
 Complex::Complex()
 {
     real = 0;
     imag = 0;
 }
 
+// to print the complex number
 void Complex::printComplex()
 {
     cout << real << " + " << imag << "i" << endl;
 }
 
+// to add two complex numbers
 void Complex::add(Complex c1, Complex c2)
 {
     real = c1.real + c2.real;
@@ -22,6 +25,7 @@ void Complex::add(Complex c1, Complex c2)
     cout << real << " + " << imag << " i " << endl;
 }
 
+// to subtract two complex numbers
 void Complex::sub(Complex c1, Complex c2)
 {
     real = c1.real - c2.real;
@@ -29,6 +33,7 @@ void Complex::sub(Complex c1, Complex c2)
     cout << real << " - " << imag << " i " << endl;
 }
 
+// to multiply two complex numbers
 // (a + ib) (c + id) = (ac - bd) + i(ad + bc)
 
 void Complex::mul(Complex c1, Complex c2)
@@ -38,6 +43,7 @@ void Complex::mul(Complex c1, Complex c2)
     cout << real << " + " << imag << " i " << endl;
 }
 
+// to divide two complex numbers
 void Complex::div(Complex c1, Complex c2)
 {
     real = (c1.real * c2.real + c1.imag * c2.imag) / (c2.real * c2.real + c2.imag * c2.imag);
@@ -45,12 +51,14 @@ void Complex::div(Complex c1, Complex c2)
     cout << real << " + " << imag << " i " << endl;
 }
 
+// to find norm of a complex number
 void Complex ::norm(Complex c1)
 {
     double r = sqrt(real * real + imag * imag);
     cout << "Norm is " << r << endl;
 }
 
+// to find conjugate of a complex number
 void Complex ::conjugate()
 {
     if (imag < 0)
